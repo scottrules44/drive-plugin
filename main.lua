a9561dda070cd60283094f1f1ba6465cb2c59799
scottrules44 <scottrules44@gmail.com>
2016-08-29 21:35:54 -0500

    2.0
    google cloud support

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
-local drive = require("plugin.googleDrive")
+local drive = require("googleDrive")
 local json = require("json")
 local canMakeRequest = false
+--drive.onlyAppAccess(true) --configure at will
 drive.init("clientId", "clientSecret", "redirectUrl")
 
 local signOutButton
@@ -34,17 +35,17 @@ requestButton.x, requestButton.y =  display.contentCenterX, display.contentCente
 requestButton.alpha = 0
 requestButton.box:addEventListener( "tap", function ( e )
 	--upload image
-	----[[
+	--[[
 	local fileName = "arrow"
 	drive.request("https://www.googleapis.com/upload/drive/v3/files/", "POST", function ( e )
-		print(json.encode(e.response))
-	end, {uploadType= "multipart"}, {name = fileName}, fileName..".png", system.ResourceDirectory, "image/png", fileName )
-	----]]
+		print(e.response)
+	end, {uploadType= "multipart"}, {name = fileName, parents ={"appDataFolder"}}, fileName..".png", system.ResourceDirectory, "image/png", fileName )
+	--]]
 	--delete file
 	--[[
 	local fileId = "0B5swSg-cH4L6cjdTWU94RTNDR0E"
 	drive.request("https://www.googleapis.com/upload/drive/v3/files/"..fileId, "DELETE", function ( e )
-		print(json.encode(e.response))
+		print(e.response)
 	end, {fileId= fileId} )
 	]]--
 	--update file
@@ -52,22 +53,44 @@ requestButton.box:addEventListener( "tap", function ( e )
 	local fileId = "0B5swSg-cH4L6cjdTWU94RTNDR0E"
 	local fileName = "arrow"
 	drive.request("https://www.googleapis.com/upload/drive/v3/files/"..fileId, "PATCH", function ( e )
-		print(json.encode(e.response))
+		print(e.response)
 	end, {fileId= fileId, uploadType= "multipart"}, {name = fileName}, fileName..".png", system.ResourceDirectory, "image/png", fileName )
 	]]--
 	--list file(s)
 	--[[
 	drive.request("https://www.googleapis.com/drive/v3/files/", "GET", function ( e )
-		print(json.encode(e.response))
-	end, {orderBy= "modifiedTime"} )
+		print(e.response)
+	end, {orderBy ="modifiedTime"} )
 	]]--
 	--download file
 	--[[
 	local fileId = ""
 	drive.download(fileId, nil, function ( e )
-		print(json.encode(e.response))
+		print(e.response)
 	end ,"arrow.png", system.TemporaryDirectory)
 	--]]
+	-------------
+	----app folder requests
+	--please note on format I recommend the ones google likes (https://developers.google.com/drive/v3/web/manage-downloads#downloading_google_documents)
+	---upload file to app data
+	--drive.createFileInAppData("arrow.png", nil--[[ default is system.ResourceDirectory ]], "image/png", function ( e )
+	--	print( e.response )
+	--end )
+	--list files in app data
+	--drive.listFilesInAppData(function ( e )
+	--	print( e.response )
+	--end)
+	--update files in app data
+	--local fileId = ""
+	--drive.updateFileInAppData(fileId,"arrow.png", nil--[[ default is system.ResourceDirectory ]], "application/vnd.google-apps.script+json", function ( e )
+	--	print( e.response )
+	--end )
+	--what about deleting and downloading? they are the same request above
+	--download file
+	--local fileId = ""
+	--drive.download(fileId, nil, function ( e )
+	--	print(json.encode(e))
+	--end ,"arrow.png", system.TemporaryDirectory)
 end )
 
 signOutButton = display.newGroup()
